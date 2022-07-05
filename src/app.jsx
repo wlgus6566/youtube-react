6306bb31d15d06b10d2285e15eb8509dbcb5e211
jihyeon <jihyeon.kim@emotion.co.kr>
2022-07-05 10:41:32 +0900

    api

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
+import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
+import SearchHeader from "./components/search_header/search_header";
 
-function App() {
+function App({youtube}) {
   const [videos, setVideos] = useState([]);
+  const search = query => {
+        youtube
+            .search(query)
+            .then(videos => setVideos(videos));
 
-  useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
     };
 
-    fetch(
-      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBi51VrAv8mK6Yel9GpXfD4XwPHuq7_g1g',
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
-  }, []);
-  return <VideoList videos={videos} />;
+  useEffect(() => {
+      youtube
+          .mostPopular()
+          .then(videos => setVideos(videos));
+  },[]);
+  return (
+      <div className={styles.app}>
+        <SearchHeader onSearch={search}/>
+        <VideoList videos={videos} />
+      </div>
+  );
 }
 
 export default App;
